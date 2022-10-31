16974ca24cf7ecceacfce483d35d4b98f7fbb4ec
Vishnu Chiluveri <vishnuviratkohli5@gmail.com>
2022-11-01 00:22:56 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,41 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
     <div>
       {/* top nav  */}
-      <div>
+      <div className="flex items-center">
         <div>
-          <img src='../assets/react.svg'></img>
+          <img></img>
+        </div>
+        <div>
+          <div></div>
+          <div>
+            <div className="text-sm">Hello</div>
+            <div className="text-sm font-bold">Select your address</div>
+          </div>
+        </div>
+        <div className="w-full appearance-none leading-tight p-3 border-2 border-black"> 
+          <input type="text" placeholder="Enter something..." />
         </div>
         <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div>
+          <div className="text-sm">Hello, sign in</div>
+          <div>
+            <div className="text-sm font-bold">Accounts & lists</div>
+            <div></div>
+          </div>
+        </div>
+        <div className="text-sm">Returns and Orders</div>
+        <div>
+          <div></div>
+          <div>Cart</div>
+        </div>
       </div>
       {/* bottom nav  */}
       <div></div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
