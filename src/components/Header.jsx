c7f3fd28fd5151305c9501905aef08cc43e3f647
Vishnu Chiluveri <vishnuviratkohli5@gmail.com>
2022-10-31 23:41:28 +0530

    imported logo

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      {/* top nav  */}
+      <div>
+        <div>
+          <img src='../assets/react.svg'></img>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      {/* bottom nav  */}
+      <div></div>
+    </div>
+  )
+}
+
+export default Header
