f828ae14dfef70a9d7509eeb986ec74fb297dccb
Vishnu Chiluveri <vishnuviratkohli5@gmail.com>
2022-11-01 20:16:55 +0530

    icons and logo added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,51 @@
 import React from "react";
+import logo from "../assets/logo.png";
+import { BsSearch } from "react-icons/bs";
+import { GoLocation } from "react-icons/go";
+import { BsCart3 } from "react-icons/bs";
 
 const Header = () => {
   return (
     <div>
       {/* top nav  */}
-      <div className="flex items-center">
+      <div className="flex items-center p-3 flex-grow border-2 border-red-300">
         <div>
-          <img></img>
+          <img src={logo} className="h-14 w-24 cursor-pointer"></img>
         </div>
         <div>
           <div></div>
-          <div>
-            <div className="text-sm">Hello</div>
-            <div className="text-sm font-bold">Select your address</div>
+          <div className="mx-5 flex items-center cursor-pointer">
+            <div><GoLocation className="w-4 h-6"/></div>
+            <div className="m-2">
+              <div className="text-sm">Hello</div>
+              <div className="text-sm font-bold cursor-pointer">
+                Select your address
+              </div>
+            </div>
           </div>
         </div>
-        <div className="w-full appearance-none leading-tight p-3 border-2 border-black"> 
-          <input type="text" placeholder="Enter something..." />
+        <div className="flex-grow">
+          <input
+            className=" w-full appearance-none leading-tight p-3 outline-none text-gray-700 bg-gray-200 focus:bg-gray-600 focus:text-white"
+            type="text"
+            placeholder="Enter something..."
+          />
         </div>
-        <div></div>
-        <div>
+        <div className="">
+          <BsSearch className="cursor-pointer w-6 h-6 ml-2" />
+        </div>
+        <div className="ml-8">
           <div className="text-sm">Hello, sign in</div>
           <div>
-            <div className="text-sm font-bold">Accounts & lists</div>
+            <div className="text-sm font-bold cursor-pointer">
+              Accounts & lists
+            </div>
             <div></div>
           </div>
         </div>
-        <div className="text-sm">Returns and Orders</div>
-        <div>
-          <div></div>
+        <div className="text-sm mx-4 cursor-pointer">Returns and Orders</div>
+        <div className="flex cursor-pointer items-center">
+          <div><BsCart3 className="w-8 h-8"/></div>
           <div>Cart</div>
         </div>
       </div>
