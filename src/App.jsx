12534c988c4dea63e873428720946d812ceaf076
Vishnu Chiluveri <vishnuviratkohli5@gmail.com>
2022-11-08 18:02:41 +0530

    created a route for sign in component and installed firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import Banner from './components/Banner'
-import Header from './components/Header'
-import ProductFeed from './components/ProductFeed'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+import ProductFeed from "./components/ProductFeed";
+import Signin from "./components/Signin";
 
 const App = () => {
   return (
-    <div className='bg-gray-100'>
+    <div className="bg-gray-100">
       <Header></Header>
-      <div className='max-w-7xl mx-auto'><Banner/></div>
-      <ProductFeed/>
+      <div className="max-w-7xl mx-auto">
+        <Banner />
+      </div>
+      <ProductFeed />
+      <Routes>
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
---
+++ src/components/Signin.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import GoogleButton from 'react-google-button'
+
+const Signin = () => {
+  return (
+    <div>
+      <h1>Sign in: </h1>
+      <GoogleButton/>
+    </div>
+  )
+}
+
+export default Signin
+
